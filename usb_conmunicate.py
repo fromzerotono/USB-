866373a56d95ddaa51e7158b392fff482a432198
金龙 刘 <3076894159@qq.com>
2026-03-20 13:38:49 +0800

    Initial commit: USB继电器Python控制代码

---
+++ usb_conmunicate.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#LINUX上运行
+#sudo pip3 install pyserial 
+#sudo python3 serialwrite.py
+import serial
+import time
+
+def usb_init(serial_port="/dev/ttyUSB0", baud_rate=9600, timeout=0.5):
+  # 打开串口并返回串口对象
+  return serial.Serial(serial_port, baud_rate, timeout=timeout)
+
+def usb_write(s, cmd):
+  # 发送 16 进制指令字符串
+  d = bytes.fromhex(cmd)
+  s.write(d)
+  print("发送命令：{0}".format(cmd))
+
+def relay_set(s, channel=1, on=True):
+  # 单函数控制开关：on=True 打开，on=False 关闭
+  op = 0x01 if on else 0x00
+  check = 0xA0 + channel + op
+  cmd = "A0 {0:02X} {1:02X} {2:02X}".format(channel, op, check)
+  usb_write(s, cmd)
+
+def usb_close(s):
+  if s and s.is_open:
+    s.close()
+
+
+if __name__ == "__main__":
+  s = usb_init()
+  try:
+    for i in range(3):
+      relay_set(s, 1, True)
+      time.sleep(3)
+
+      relay_set(s, 1, False)
+      # 给继电器保持断开状态的时间，避免下一轮立即又打开
+      time.sleep(0.5)
+  finally:
+    usb_close(s)
